e8d39c4d055503aa468809e879b4a9221c48c09d
Eugene Triguba <eugenetriguba@gmail.com>
2020-09-30 11:15:53 -0500

    Fix homework 5

--- Advanced Programming/hm5/AboveBelowAverage.java
+++ Advanced Programming/hm5/AboveBelowAverage.java
@@ -9,20 +9,18 @@ public class AboveBelowAverage {
     public static void main(String[] args) {
         int[] arr = new Random().ints(15, 1, 101).sorted().toArray();
         double avg = IntStream.of(arr).average().getAsDouble();
-        int[] aboveAvg = IntStream.of(arr).filter(x -> x < avg).toArray();
-        int[] belowAvg = IntStream.of(arr).filter(x -> x > avg).toArray();
+        int[] aboveAvg = IntStream.of(arr).filter(x -> x > avg).toArray();
+        int[] belowAvg = IntStream.of(arr).filter(x -> x < avg).toArray();
 
-        System.out.println("15 random numbers in ascending order: " + formatIntStream(IntStream.of(arr)));
+        System.out.println("15 random numbers in ascending order: " + formatIntStream(arr));
         System.out.printf("Average: %.2f\n", avg);
-        System.out.println(IntStream.of(aboveAvg).count() + " numbers above the average: "
-                + formatIntStream(IntStream.of(aboveAvg)));
-        System.out.println(IntStream.of(belowAvg).count() + " numbers below the average: "
-                + formatIntStream(IntStream.of(belowAvg)));
+        System.out.println(aboveAvg.length + " numbers above the average: " + formatIntStream(aboveAvg));
+        System.out.println(belowAvg.length + " numbers below the average: " + formatIntStream(belowAvg));
     }
 
     // Format a stream of ints into a String of numbers
     // each seperated by a single space.
-    private static String formatIntStream(IntStream is) {
-        return is.mapToObj(String::valueOf).collect(Collectors.joining(" "));
+    private static String formatIntStream(int[] arr) {
+        return IntStream.of(arr).mapToObj(String::valueOf).collect(Collectors.joining(" "));
     }
 }
